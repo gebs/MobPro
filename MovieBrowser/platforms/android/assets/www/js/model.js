41de5f1c5f07fb5276540c775907dd0bfb280662
psychonoob <adrian.gebs@stud.hslu.ch>
2017-04-25 00:18:04 +0200

    Implementierung Movie Browser

---
+++ MovieBrowser/platforms/android/assets/www/js/model.js
@@ -0,0 +1,121 @@
+var favorites =  [
+    {  
+   Title:"Deadpool",
+   Year:"2016",
+   Rated:"R",
+   Released:"12 Feb 2016",
+   Runtime:"108 min",
+   Genre:"Action, Adventure, Comedy",
+   Director:"Tim Miller",
+   Writer:"Rhett Reese, Paul Wernick",
+   Actors:"Ryan Reynolds, Karan Soni, Ed Skrein, Michael Benyaer",
+   Plot:"A fast-talking mercenary with a morbid sense of humor is subjected to a rogue experiment that leaves him with accelerated healing powers and a quest for revenge.",
+   Language:"English",
+   Country:"USA",
+   Awards:"Nominated for 2 Golden Globes. Another 19 wins & 57 nominations.",
+   Poster:"https://images-na.ssl-images-amazon.com/images/M/MV5BMjQyODg5Njc4N15BMl5BanBnXkFtZTgwMzExMjE3NzE@._V1_SX300.jpg",
+   Ratings:[  
+      {  
+         Source:"Internet Movie Database",
+         Value:"8.0/10"
+      },
+      {  
+         Source:"Rotten Tomatoes",
+         Value:"84%"
+      },
+      {  
+         Source:"Metacritic",
+         Value:"65/100"
+      }
+   ],
+   Metascore:"65",
+   imdbRating:"8.0",
+   imdbVotes:"612,933",
+   imdbID:"tt1431045",
+   Type:"movie",
+   DVD:"10 May 2016",
+   BoxOffice:"$328,674,489.00",
+   Production:"20th Century Fox",
+   Website:"http://www.foxmovies.com/deadpool",
+   Response:"True"
+},{  
+   Title:"Rogue One",
+   Year:"2016",
+   Rated:"PG-13",
+   Released:"16 Dec 2016",
+   Runtime:"133 min",
+   Genre:"Action, Adventure, Sci-Fi",
+   Director:"Gareth Edwards",
+   Writer:"Chris Weitz (screenplay), Tony Gilroy (screenplay), John Knoll (story by), Gary Whitta (story by), George Lucas (based on characters created by)",
+   Actors:"Felicity Jones, Diego Luna, Alan Tudyk, Donnie Yen",
+   Plot:"The Rebel Alliance makes a risky move to steal the plans for the Death Star, setting up the epic saga to follow.",
+   Language:"English",
+   Country:"USA, UK",
+   Awards:"Nominated for 2 Oscars. Another 8 wins & 68 nominations.",
+   Poster:"https://images-na.ssl-images-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_SX300.jpg",
+   Ratings:[  
+      {  
+         Source:"Internet Movie Database",
+         Value:"8.0/10"
+      },
+      {  
+         Source:"Rotten Tomatoes",
+         Value:"85%"
+      },
+      {  
+         Source:"Metacritic",
+         Value:"65/100"
+      }
+   ],
+   Metascore:"65",
+   imdbRating:"8.0",
+   imdbVotes:"286,861",
+   imdbID:"tt3748528",
+   Type:"movie",
+   DVD:"N/A",
+   BoxOffice:"$531,314,491.00",
+   Production:"Walt Disney Pictures",
+   Website:"http://www.starwars.com/",
+   Response:"True"
+},
+{  
+   Title:"Moana",
+   Year:"2016",
+   Rated:"PG",
+   Released:"23 Nov 2016",
+   Runtime:"107 min",
+   Genre:"Animation, Adventure, Comedy",
+   Director:"Ron Clements, Don Hall, John Musker, Chris Williams",
+   Writer:"Jared Bush (screenplay), Ron Clements (story by), John Musker (story by), Chris Williams (story by), Don Hall (story by), Pamela Ribon (story by), Aaron Kandell (story by), Jordan Kandell (story by)",
+   Actors:"Auli'i Cravalho, Dwayne Johnson, Rachel House, Temuera Morrison",
+   Plot:"In Ancient Polynesia, when a terrible curse incurred by the Demigod Maui reaches an impetuous Chieftain's daughter's island, she answers the Ocean's call to seek out the Demigod to set things right.",
+   Language:"English",
+   Country:"USA",
+   Awards:"Nominated for 2 Oscars. Another 11 wins & 66 nominations.",
+   Poster:"https://images-na.ssl-images-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_SX300.jpg",
+   Ratings:[  
+      {  
+         Source:"Internet Movie Database",
+         Value:"7.7/10"
+      },
+      {  
+         Source:"Rotten Tomatoes",
+         Value:"95%"
+      },
+      {  
+         Source:"Metacritic",
+         Value:"81/100"
+      }
+   ],
+   Metascore:"81",
+   imdbRating:"7.7",
+   imdbVotes:"93,475",
+   imdbID:"tt3521164",
+   Type:"movie",
+   DVD:"07 Mar 2017",
+   BoxOffice:"$248,558,024.00",
+   Production:"Walt Disney Pictures",
+   Website:"http://movies.disney.com/moana",
+   Response:"True"
+}
+]
---
+++ MovieBrowser/www/js/movies.js
@@ -0,0 +1,48 @@
+function SearchMovie(){
+    var searchval = htmlEncode($("#txtSearch").val());
+    $.get("http://www.omdbapi.com/?y=&plot=short&r=json&t="+ searchval,function(data){
+        if (data != undefined){
+            fillMovie(data);
+        }
+    });
+
+}
+fillFavorites();
+$("#btnSearch").click(function(){
+    SearchMovie();
+});
+function fillFavorites(){
+    $("#favList").empty();
+    $.each(favorites,function(i,a){
+        $("#favList").append("<li><a id=\"fav_" + i+ "\">" + a.Title+"</a></li>")
+        $("#fav_" + i).click(function(){
+            fillFavorite(i);
+        });
+    });
+}
+function fillFavorite(id){
+    fillMovie(favorites[id]);
+}
+function fillMovie(movie)
+{
+    $("#txtTitle").val();
+    $("#txtYear").val();
+    $("#txtActors").empty();
+    $("#txtPlot").empty();
+    $("#txtRating").val();
+
+    $("#txtTitle").val(movie.Title);
+    $("#txtYear").val(movie.Year);
+    $("#txtActors").val(movie.Actors);
+    $("#txtPlot").val(movie.Plot);
+    $("#txtRating").val(movie.Ratings[0].Value);
+    $("#imgPoster").attr("src",movie.Poster);
+
+    $("body").pagecontainer("change", "#page-movie", {  });
+
+}
+function htmlEncode(value){
+  //create a in-memory div, set it's inner text(which jQuery automatically encodes)
+  //then grab the encoded contents back out.  The div never exists on the page.
+  return $('<div/>').text(value).html();
+}
